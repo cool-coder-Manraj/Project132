8e4dee3699b2370d898e965ac4aacd1ff0c99e30
cool-coder-Manraj <94151797+cool-coder-Manraj@users.noreply.github.com>
2022-10-03 19:01:45 -0700

    Add files via upload

---
+++ couch.js
@@ -0,0 +1,32 @@
+status = "";
+
+function preload()
+{
+    img = loadImage('IMG-2281.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+
+    document.getElementById("status").innerHTML = "Status; detecting objects .......";
+}
+
+function modelLoaded()
+{
+    console.log("Model is Loaded :)");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
